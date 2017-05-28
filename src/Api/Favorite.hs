0edfc33daae053746de0e15fae62fbfdad9bf5b1
Rui Afonso Pereira <rpereira@users.noreply.github.com>
2017-05-29 00:26:45 +0100

    Unfavorite article

--- src/Api/Favorite.hs
+++ src/Api/Favorite.hs
@@ -17,12 +17,17 @@ import Models
 import Types
 
 type FavoriteAPI =
-    "articles" :> Capture "slug" Slug
-               :> "favorite"
-               :> Post '[JSON] (Art (Entity Article))
+         "articles" :> Capture "slug" Slug
+                    :> "favorite"
+                    :> Post '[JSON] (Art (Entity Article))
+
+    :<|> "articles" :> Capture "slug" Slug
+                    :> "favorite"
+                    :> Delete '[JSON] (Art (Entity Article))
 
 favoriteServer :: ServerT FavoriteAPI App
 favoriteServer = favoriteArticle
+            :<|> unfavoriteArticle
 
 -- TODO: We mock a specific user when marking an article as favorite - userId
 -- should come from session.
@@ -33,4 +38,16 @@ favoriteArticle slug = do
       Nothing -> throwError err404
       Just article -> do
           runDb $ insert (Favorite (toSqlKey 1) (entityKey article))
+          return $ Art article
+
+-- TODO: We mock a specific user when unmarking an article as favorite - userId
+-- should come from session.
+unfavoriteArticle :: Slug -> App (Art (Entity Article))
+unfavoriteArticle slug = do
+    maybeArticle <- runDb $ selectFirst [ArticleSlug ==. slug] []
+    case maybeArticle of
+      Nothing -> throwError err404
+      Just article -> do
+          runDb $ deleteWhere [ FavoriteUserId ==. toSqlKey 1
+                              , FavoriteArticleId ==. entityKey article ]
           return $ Art article
