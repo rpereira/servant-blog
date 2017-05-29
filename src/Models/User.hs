759ba9984e9773b0c4ef8a153f06ed70c288b39c
Rui Afonso Pereira <rpereira@users.noreply.github.com>
2017-05-29 15:48:56 +0100

    Do not expose user private data when returning a profile

--- src/Models/User.hs
+++ src/Models/User.hs
@@ -1,25 +1,24 @@
-{-# LANGUAGE DeriveGeneric              #-}
 {-# LANGUAGE EmptyDataDecls             #-}
-{-# LANGUAGE FlexibleContexts           #-}
 {-# LANGUAGE FlexibleInstances          #-}
 {-# LANGUAGE GADTs                      #-}
 {-# LANGUAGE GeneralizedNewtypeDeriving #-}
 {-# LANGUAGE MultiParamTypeClasses      #-}
 {-# LANGUAGE OverloadedStrings          #-}
 {-# LANGUAGE QuasiQuotes                #-}
-{-# LANGUAGE RecordWildCards            #-}
 {-# LANGUAGE TemplateHaskell            #-}
 {-# LANGUAGE TypeFamilies               #-}
 
 module Models.User where
 
-import Data.Text           (Text)
-import Data.Time           (UTCTime)
-import Database.Persist.TH (mkMigrate, mkPersist, persistLowerCase, share,
-                            sqlSettings)
+import Data.Aeson                  (ToJSON, object, toJSON, (.=))
+import Data.Text                   (Text)
+import Data.Time                   (UTCTime)
+import Database.Persist.Postgresql (Entity (..), entityVal)
+import Database.Persist.TH         (mkMigrate, mkPersist, persistLowerCase,
+                                    share, sqlSettings)
 
 share [mkPersist sqlSettings, mkMigrate "migrateUser"] [persistLowerCase|
-User json sql=users
+User sql=users
     username  Text
     email     Text
     bio       Text Maybe default=NULL
@@ -30,3 +29,11 @@ User json sql=users
     UniqueUser username email
     deriving Show
 |]
+
+instance ToJSON (Entity User) where
+    toJSON entity = object
+        [ "username" .= userUsername val
+        , "bio"      .= userBio val
+        , "image"    .= userImage val
+        ]
+            where val = entityVal entity
