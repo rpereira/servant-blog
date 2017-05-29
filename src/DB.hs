0b517e2381bbf47eb7c7078a6f2dd54b7a920757
Rui Afonso Pereira <rpereira@users.noreply.github.com>
2017-05-29 14:34:34 +0100

    Provide a list of migrations to run

--- src/DB.hs
+++ src/DB.hs
@@ -25,16 +25,21 @@ import Models.Tagging
 import Models.User
 import Models.UserFollower
 
+-- | List of database migrations to perform.
+migrations =
+    [ migrateArticle
+    , migrateComment
+    , migrateFavorite
+    , migrateUser
+    , migrateUserFollower
+    , migrateTag
+    , migrateTagging
+    ]
+
 -- | Perform database migrations.
 runMigrations :: SqlPersistT IO ()
-runMigrations = do
-    runMigration migrateArticle
-    runMigration migrateComment
-    runMigration migrateFavorite
-    runMigration migrateUser
-    runMigration migrateUserFollower
-    runMigration migrateTag
-    runMigration migrateTagging
+runMigrations = forM_ migrations $ \m ->
+    runMigration m
 
 -- | Run database actions.
 runDb :: (MonadReader Config m, MonadIO m) => SqlPersistT IO b -> m b
