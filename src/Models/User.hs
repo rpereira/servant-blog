bfb9ef6d5a09e2c4dcf21d9d8b9028aaa23ce16f
Rui Afonso Pereira <rpereira@users.noreply.github.com>
2017-05-30 23:10:04 +0100

    Revert "Do not expose user private data when returning a profile"
    This reverts commit 759ba9984e9773b0c4ef8a153f06ed70c288b39c.

--- src/Models/User.hs
+++ src/Models/User.hs
@@ -1,24 +1,25 @@
+{-# LANGUAGE DeriveGeneric              #-}
 {-# LANGUAGE EmptyDataDecls             #-}
+{-# LANGUAGE FlexibleContexts           #-}
 {-# LANGUAGE FlexibleInstances          #-}
 {-# LANGUAGE GADTs                      #-}
 {-# LANGUAGE GeneralizedNewtypeDeriving #-}
 {-# LANGUAGE MultiParamTypeClasses      #-}
 {-# LANGUAGE OverloadedStrings          #-}
 {-# LANGUAGE QuasiQuotes                #-}
+{-# LANGUAGE RecordWildCards            #-}
 {-# LANGUAGE TemplateHaskell            #-}
 {-# LANGUAGE TypeFamilies               #-}
 
 module Models.User where
 
-import Data.Aeson                  (ToJSON, object, toJSON, (.=))
-import Data.Text                   (Text)
-import Data.Time                   (UTCTime)
-import Database.Persist.Postgresql (Entity (..), entityVal)
-import Database.Persist.TH         (mkMigrate, mkPersist, persistLowerCase,
-                                    share, sqlSettings)
+import Data.Text           (Text)
+import Data.Time           (UTCTime)
+import Database.Persist.TH (mkMigrate, mkPersist, persistLowerCase, share,
+                            sqlSettings)
 
 share [mkPersist sqlSettings, mkMigrate "migrateUser"] [persistLowerCase|
-User sql=users
+User json sql=users
     username  Text
     email     Text
     bio       Text Maybe default=NULL
@@ -29,11 +30,3 @@ User sql=users
     UniqueUser username email
     deriving Show
 |]
-
-instance ToJSON (Entity User) where
-    toJSON entity = object
-        [ "username" .= userUsername val
-        , "bio"      .= userBio val
-        , "image"    .= userImage val
-        ]
-            where val = entityVal entity
