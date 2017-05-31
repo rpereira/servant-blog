7e3cdd76cc7abc9831133bbf4903adf83ba76809
Rui Afonso Pereira <rpereira@users.noreply.github.com>
2017-05-31 08:26:43 +0100

    Delete comment in specified article

--- src/Api/Comment.hs
+++ src/Api/Comment.hs
@@ -4,12 +4,16 @@
 
 module Api.Comment where
 
-import Database.Persist.Postgresql (Entity (..), rawSql, toPersistValue, (==.))
+import Data.Int                    (Int64)
+import Database.Persist.Postgresql (Entity (..), deleteWhere, rawSql,
+                                    selectFirst, toPersistValue, toSqlKey,
+                                    (==.))
 import Database.Persist.Types      (SelectOpt (..))
 import Servant
 
 import Config                      (App (..))
 import DB                          (runDb)
+import Models.Article
 import Models.Comment
 import Types
 
@@ -18,8 +22,15 @@ type CommentAPI =
                     :> "comments"
                     :> Get '[JSON] (Cmts [Entity Comment])
 
+    :<|> "articles" :> Capture "slug" Slug
+                    :> "comments"
+                    :> Capture "articleId" Int64
+                    :> DeleteNoContent '[JSON] NoContent
+
 commentServer :: ServerT CommentAPI App
-commentServer = getCommentsForArticle
+commentServer =
+         getCommentsForArticle
+    :<|> deleteCommentInArticle
 
 -- | TODO: the returned object does not comply with the API spec. we need to
 -- remove the article_id and add the field author with info about the user who
@@ -31,3 +42,13 @@ getCommentsForArticle slug = do
         where stm = "SELECT ?? FROM comments \
                     \WHERE article_id IN (\
                         \SELECT id FROM articles WHERE slug = ?)"
+
+deleteCommentInArticle :: Slug -> Int64 -> App NoContent
+deleteCommentInArticle slug commentId = do
+    maybeArticle <- runDb $ selectFirst [ArticleSlug ==. slug] []
+    case maybeArticle of
+      Nothing -> throwError err404
+      Just article -> do
+          runDb $ deleteWhere [ CommentArticleId ==. entityKey article
+                              , CommentId ==. toSqlKey commentId ]
+          return NoContent
